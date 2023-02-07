aab83041af82ca46e8b4f6691601c0a38da59235
unknown <ranazubair696@gmail.com>
2023-02-07 19:41:18 +0500

    css working

---
+++ baseurl.js
@@ -0,0 +1,14 @@
+function apiUrl(){
+    return "http://localhost:3003/";
+}
+
+
+function imgUrl(){
+    return "http://localhost/ijvs_admin/upload/";
+}
+
+
+
+export  {apiUrl,imgUrl};
+
+// export {apiUrl,imgurl};
