d366ccf5989b9f34912aba09acc755c06a221c23
Yasir Memood <yasirmehmood34500@gmail.com>
2023-02-09 21:57:29 +0500

    copy of v1.4

--- pages/archive.js
+++ pages/archive.js
@@ -5,53 +5,53 @@ import RightMenu from '../components/RightSide'
 import Axios from "axios";
 import { apiUrl } from "../baseurl";
 
-const archive = ({archive}) => {
-  return (
-    <>
-        <div>
-            <NavOne />
-        </div>
-        <div className='row p-4'>
-            <div className='col-lg-9'>
-                <h2 className="text-center">Archive</h2>
-                <hr className="hrbgline"></hr>
-                {archive.map((a, i) => (
-                    <div className='p-2' key={i}>
-                    <div className='text-center'><h3 className='h3fontsiz'>Year {a.year}</h3></div>
-                    <div className='mt-2'>
-                        <div className='row'>
-                            <div className='mntbg bg-primary text-white'><strong>January-March</strong></div>
-                            <div className='mntbg bg-primary text-white'><strong>April-June</strong></div>
-                            <div className='mntbg bg-primary text-white'><strong>July-September</strong></div>
-                            <div className='mntbg bg-primary text-white'><strong>October-December</strong></div>
-                            {a.volumes.map((v,ii) => (
-                                <div key={ii} className='mntbg'><strong><Link href={'archivevolumeview/'+v.id}><a>{v.volumeNo}</a></Link></strong></div>
-                            ))}
-                                
+const archive = ({ archive }) => {
+    return (
+        <>
+            <div>
+                <NavOne />
+            </div>
+            <div className='row p-4'>
+                <div className='col-lg-9'>
+                    <h2 className="text-center">Archive</h2>
+                    <hr className="hrbgline"></hr>
+                    {archive.map((a, i) => (
+                        <div className='p-2' key={i}>
+                            <div className='text-center'><h3 className='h3fontsiz'>Year {a.year}</h3></div>
+                            <div className='mt-2'>
+                                <div className='row'>
+                                    <div className='mntbg bg-primary text-white'><strong>January-March</strong></div>
+                                    <div className='mntbg bg-primary text-white'><strong>April-June</strong></div>
+                                    <div className='mntbg bg-primary text-white'><strong>July-September</strong></div>
+                                    <div className='mntbg bg-primary text-white'><strong>October-December</strong></div>
+                                    {a.volumes.map((v, ii) => (
+                                        <div key={ii} className='mntbg'><strong><Link href={'archivevolumeview/' + v.id}><span>{v.volumeNo}</span></Link></strong></div>
+                                    ))}
+
+                                </div>
+                                <hr></hr>
+                                {/* <Link href={'archivevolumeview'}><span>Volume 12, No. 2, 2023 (In Progress)</span></Link>                         */}
                             </div>
-                            <hr></hr>
-                        {/* <Link href={'archivevolumeview'}><a>Volume 12, No. 2, 2023 (In Progress)</a></Link>                         */}
-                    </div>
+                        </div>
+                    ))}
+
+
                 </div>
-                ))}
-                
-                
+                <div className='col-lg-3'>
+                    <RightMenu />
+                </div>
             </div>
-            <div className='col-lg-3'>
-                <RightMenu />
+            <div>
+                <FooterOne />
             </div>
-        </div>
-        <div>
-            <FooterOne />
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export async function getServerSideProps() {
     const res = await fetch(apiUrl() + "yearVolume/view")
     const archive = await res.json();
     return { props: { archive } }
-  }
+}
 
 export default archive
